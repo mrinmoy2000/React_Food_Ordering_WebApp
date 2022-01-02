2d5474cb7a548c6ac53b14f8c4b796fe2d05c97e
Mrinmoy <mrin2ksarkar@gmail.com>
2022-01-02 18:14:20 +0530

    Initial Commits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Fragment } from 'react'
 import Header from "./components/Layout/Header";
+import AvailableMeals from './components/Meals/AvailableMeals';
+import MealsSummary from './components/Meals/MealsSummary';
+
 function App() {
   return (
     <Fragment>
       <Header />
+      <MealsSummary />
+      <AvailableMeals />
     </Fragment>
   );
 }
