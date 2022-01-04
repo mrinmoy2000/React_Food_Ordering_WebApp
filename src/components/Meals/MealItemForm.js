debdbe20fea1369a6983002e5c76e378e47220f5
Mrinmoy <mrin2ksarkar@gmail.com>
2022-01-04 22:18:48 +0530

    Initial Changes

--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,18 +1,38 @@
 import classes from "./MealItemForm.module.css";
 import Input from "../UI/Input";
+import { useRef, useState } from "react";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true)
+  const amountInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNum = +enteredAmount;
+
+    if(enteredAmount.trim().length === 0 || enteredAmountNum < 1 || enteredAmountNum > 5){
+      setAmountIsValid(false)
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNum)
+  };
   return (
-    <form className={classes.form}>
-      <Input label="Amount" input={{
-        id: 'amount_' + props.id,
-        type: 'number',
-        min: '0',
-        max: '5',
-        step: '1',
-        defaultValue: '0'
-      }}/>
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please Enter a valid amount</p>}
     </form>
   );
 };
--- src/components/store/CartProvider.js
+++ src/components/store/CartProvider.js
@@ -1,21 +1,88 @@
-import CartContext from "./cart-context"; 
+import CartContext from "./cart-context";
+import { useReducer } from "react";
 
-const CartProvider = props => {
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-    const addItemToCartHandler = item => {}
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const existingItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingItemIndex];
+    let updatedItems;
 
-    const removeItemToCartHandler = id => {}
-
-    const cartContext = {
-        items: [],
-        totalAmount: 0,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemToCartHandler
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
     }
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
 
-    return <CartContext.Provider value={cartContext}>
-        {props.children}
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+    let updatedItems;
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemToCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemToCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
     </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
